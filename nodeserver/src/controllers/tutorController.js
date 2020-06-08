6f2b8aba9a3d1fd821bc03fc6718b9f1d287ad08
Jon Echeveste Gonzalez <echevestegonzalezjon@gmail.com>
2020-06-08 09:21:29 +0100

    added tutor to models

---
+++ nodeserver/src/controllers/tutorController.js
@@ -0,0 +1,98 @@
+const Tutor = require('../models/Tutor');
+
+/**
+ * SYNC ALL MODELS WITH DB
+ */
+//sequelize.sync();
+Tutor.sync({ force: false })
+    .then(() => {
+        console.log('SYNC MODEL TUTOR');
+    });
+
+const tutorController = {};
+
+tutorController.getAll = async (req, res) => {
+    Tutor.findAll()
+        .then(each => {
+            const data = JSON.parse(JSON.stringify(each));
+            
+            return res.json({ success: true, data: data });
+        }).catch(err => {
+            console.log(err)
+        })
+};
+
+tutorController.getId = (req, res) => {
+    const { id } = req.params;
+    Tutor.findAll({ where: { id } })
+        .then(each => {
+            if (each.length > 0) {
+                const data = JSON.parse(JSON.stringify(each));
+                
+                return res.json({ success: true, data: data });
+            } else {
+                res.json({ status: `The tutor doesn't exist` });
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+};
+
+tutorController.add = (req, res) => {
+    const { name } = req.body;
+    Tutor.create({ name })
+        .then(each => {
+            if (each.id) {
+                res.json({ success: true, message: `Successfully added, id: ${each.id}` });
+            } else {
+                res.json({ status: 'Error' });
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+}
+
+tutorController.edit = (req, res) => {
+    //const {id}=req.params;
+    const { id, name } = req.body;
+    
+    Tutor.update({ name },
+        { where: { id } })
+        .then(each => {
+            const data = JSON.parse(JSON.stringify(each));
+            if (data.length > 0) {
+                res.json({ success: true, message: 'Successfully updated' });
+            } else {
+                res.json({ status: 'Error' });
+            }
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+
+}
+
+
+tutorController.delete = (req, res) => {
+    const { id } = req.params;
+    Tutor.destroy({ where: { id } })
+        .then(each => {
+            const data = JSON.parse(JSON.stringify(each));
+            if (data == 1) {
+                res.json({ success: true, message: 'Succesfully deleted' });
+            } else {
+                res.json({ status: 'Error' });
+            }
+
+        })
+        .catch(err => {
+            console.log(err)
+        });
+
+}
+module.exports = tutorController;
---
+++ nodeserver/src/routes/tutorRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const tutorController = require('../controllers/tutorController');
+
+
+//GET ALL
+router.get('/tutor/list', tutorController.getAll);
+
+//GET WITH ID
+router.get('/tutor/:id',tutorController.getId);
+
+//CREATE
+router.post('/tutor', tutorController.add);
+
+//DELETE
+router.delete('/tutor/:id', tutorController.delete);
+
+//UPDATE 
+router.put('/tutor',tutorController.edit);
+
+module.exports=router;
