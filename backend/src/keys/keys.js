be8be843596cea91937d43fb5b18ac5e4f81bc0a
Jon Echeveste González <echevestegonzalezjon@gmail.com>
2020-08-10 13:50:35 +0100

    Update keys.js

--- backend/src/keys/keys.js
+++ backend/src/keys/keys.js
@@ -17,10 +17,10 @@
 
 const KeysDatabase = ({
   host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'datesdb',
-  port: 3307,
+  user: 'dates',
+  password: 'your_password',
+  database: 'dates_db',
+  port: 3306,
   dialect: 'mysql'
 });
 
